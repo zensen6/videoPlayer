4f107d1752fd85d766a9759b62eec1e86aa047f2
zensen6 <junhee1469@gmail.com>
2022-01-02 19:10:46 +0900

    more chages

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 
 const app = express();
 app.get('/', (req, res) => {
-	res.send('123');
+	res.send('123123');
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
 	console.log(`server iss running on http://localhost:${PORT}`);
 });
+
+// ts-node-dev: nodemon + ts-node
+// ts-node: node xxx 할때 단순히 ts파일 node하는 거라 보면된다.
+
+// npm install express cors mongoose morgan dotenv
+// yarn add -D express typescript ts-node nodemon @types/node @types/express ts-node-dev
