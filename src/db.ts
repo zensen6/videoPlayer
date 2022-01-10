d632e40a3669b487138e6f97efb327ba202c22d1
zensen6 <junhee1469@gmail.com>
2022-01-10 11:55:42 +0900

    replace @types/mongoose with older mongoose version and solve the useUnifiediTopology problem

---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { triggerAsyncId } from 'async_hooks';
+
+(async () =>
+	mongoose.connect('mongodb://127.0.0.1:27017/tsnode', {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	}))();
+
+const db = mongoose.connection;
+const handleOpen = () => console.log('OPEN');
+db.on('error', (err) => {
+	console.log(`err:${err}`);
+});
+db.once('open', handleOpen);
